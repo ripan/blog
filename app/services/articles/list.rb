9893cb35620a1fef10b711f42f830c7ec5ebfb99
Ripan <er.ripankumar@gmail.com>
2023-02-23 00:31:54 +0000

    fix: return article likes_count with articles list

--- app/services/articles/list.rb
+++ app/services/articles/list.rb
@@ -1,6 +1,17 @@
 class Articles::List < Articles::Base
   def call
     data = api_request(API_URL, options)
-    api_response(data)
+    response = api_response(data)
+    response.map!{ |article| article.merge(likes_count: likes_group[article["id"]]&.first&.count) }
+  end
+
+  private
+
+  def likes
+    Like.where(resource_type: "Article")
+  end
+
+  def likes_group
+    likes.group_by(&:resource_id)
   end
 end
